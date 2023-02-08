99b0456c98ebb02668a7b978ddc6fa7719a0aafe
montescar19dev <carlosmontes19.dev@gmail.com>
2023-02-08 00:56:45 -0400

    posts CRUD finished

--- src/models/posts.models.js
+++ src/models/posts.models.js
@@ -1,3 +1,27 @@
-const Posts = 'something'
+const { DataTypes } = require('sequelize')
+
+const db = require('../utils/database')
+
+const Posts = db.define('posts', {
+    id: {
+        type: DataTypes.BIGINT,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    isPublished: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+    }
+
+})
 
 module.exports = Posts
--- src/posts/posts.controllers.js
+++ src/posts/posts.controllers.js
@@ -1,27 +1,46 @@
+const Posts = require('../models/posts.models')
 
 const findAllPosts = async() => {
-    //? Your code here:
+    const data = await Posts.findAll()
+    return data
+}
+
+const findPostById = async(id) => {
+    const data = await Posts.findOne({
+        where: {
+            id : id
+        }
+    })
+    return data
+}
+
+const createPost = async(postInf) => {
+    const newPost = {
+        content: postInf.content,
+        userName: postInf.userName,
+        isPublished: postInf.isPublished
+    }
+    const data = await Posts.create(newPost)
+    return data
+}
+
+const updatePost = async(id, postInf) => {
+    const data = await Posts.update(postInf, {
+        where: {
+            id : id
+        }
+    })
+    return data
 
 }
 
-const findPostById = async() => {
-    //? Your code here:
-
-}
-
-const createPost = async() => {
-    //? Your code here:
-
-}
-
-const updatePost = async() => {
-    //? Your code here:
-
-}
-
-const deletePost = async() => {
-    //? Your code here:
-
+const deletePost = async(id) => {
+    const data = await Posts.destroy({
+        where: {
+            id : id
+        }
+    })
+    return data
 }
 
 module.exports = {
